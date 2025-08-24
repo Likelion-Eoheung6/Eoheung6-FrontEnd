926a1bd91efd86194803945a461a28f8299a26e8
binniejung <ybe1027@naver.com>
2025-08-24 18:56:23 +0900

    🎨 Style: 카카오톡 이미지 수정

--- src/components/login/KakaoLoginButton.tsx
+++ src/components/login/KakaoLoginButton.tsx
@@ -1,4 +1,4 @@
-import KakaoIcon from '../../assets/login/kakao.svg';
+import KakaoIcon from '../../assets/login/kakao2.svg';
 
 export default function KakaoLoginButton() {
   return (
@@ -7,11 +7,11 @@ export default function KakaoLoginButton() {
         type="button"
         className="relative rounded-[30px] bg-[#FEE102] shadow-[0_4px_4px_2px_rgba(0,0,0,0.04)] flex items-center justify-center gap-[5px] appearance-none border-0 outline-none focus:outline-none ring-0 focus:ring-0 cursor-pointer px-[66px] py-[6px]"
       >
-        <img src={KakaoIcon} alt="kakao" className="w-[32px] h-[32px]" />
-        <span className="text-[#545454] text-[14px] font-medium leading-[1.2] tracking-[-0.025em]">카카오톡으로 로그인하기</span>
+        <img src={KakaoIcon} alt="kakao" className="w-[28px] h-[28px]" />
+        <span className="text-[#545454] text-[14px] font-medium leading-[1.2] tracking-[-0.025em]">
+          카카오톡으로 로그인하기
+        </span>
       </button>
     </div>
   );
 }
-
-
